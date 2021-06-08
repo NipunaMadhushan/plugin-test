573a8665e43bd7dbc7952d2f7b10e310c98d8d99
Nipuna Madhushan <51471998+NipunaMadhushan@users.noreply.github.com>
2021-06-08 13:48:36 +0530

    Add files via upload

---
+++ src/main/groovy/com/example/plugins/filediff/FileDiffPlugin.groovy
@@ -0,0 +1,17 @@
+package com.example.plugins.filediff
+
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class FileDiffPlugin implements Plugin<Project> {
+    @Override
+    void apply(Project project) {
+        project.extensions.create("fileDiff", FileDiffExtension)
+        project.tasks.create("fileDiff", FileDiffTask) {
+            file1 = project.fileDiff.file1
+            file2 = project.fileDiff.file2
+        }
+
+    }
+}
---
+++ src/test/groovy/com/example/plugins/filediff/FileDiffPluginIntegrationTest.groovy
@@ -0,0 +1,48 @@
+package com.example.plugins.filediff
+
+import org.gradle.testkit.runner.GradleRunner
+import org.gradle.testkit.runner.TaskOutcome
+import org.junit.Rule
+import org.junit.rules.TemporaryFolder
+import spock.lang.Specification
+
+class FileDiffPluginIntegrationTest extends Specification {
+    @Rule
+    TemporaryFolder testProjectDir = new TemporaryFolder()
+
+    File buildFile
+
+    def setup() {
+        buildFile = testProjectDir.newFile('build.gradle')
+
+        buildFile << """
+            plugins {
+                id 'gradle'
+            }
+        """
+    }
+
+    def "can successfully diff two files"() {
+        given:
+        File testFile1 = testProjectDir.newFile('testFile1.txt')
+        File testFile2 = testProjectDir.newFile('testFile2.txt')
+
+        buildFile << """
+            fileDiff {
+                file1 = file(${testFile1.getName()})
+                file2 = file(${testFile2.getName()})
+            }
+            
+        """
+        when:
+        def result = GradleRunner.create()
+                .withProjectDir(testProjectDir.root)
+                .withArguments('fileDiff')
+                .withPluginClasspath()
+                .build()
+        then:
+        result.output.contains('Files have the same size')
+        result.task(':fileDiff').outcome == TaskOutcome.SUCCESS
+    }
+
+}
